16ab06610a82114533d5461ae0615e0c1e8aa327
Zeeshan Rais <zeeshanrais@Zeeshans-MacBook-Pro.local>
2023-08-19 21:39:51 +0300

    first prog

---
+++ Assignment-01/01 - Personal Message/main.js
@@ -0,0 +1,2 @@
+var person_name = 123;
+console.log("Hello ".concat(person_name, ", would you like to learn some Python today?"));
---
+++ Assignment-01/01 - Personal Message/test.js
@@ -0,0 +1,17 @@
+var age;
+age=21;
+
+if(age>12){
+    console.log('in if cond');
+    console.log('adult');
+}
+else if(age<12){
+    console.log('in else-if cond');
+    console.log('toddler');
+}
+else{
+    console.log('else ');
+    console.log("teen");
+}
+
+
